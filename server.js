e137bc382cc1208bf8053a742b343152d6b0b3eb
BSRCIPHER <121090093+BSRCIPHER@users.noreply.github.com>
2026-01-07 13:15:22 +0530

    corse removed

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { swaggerUi, swaggerSpec } = require('./swagger');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 // Serve static files from public directory
